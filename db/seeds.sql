e62cc1c49364449a6d52ee2a46305bcd024ca646
Elaine Danahy <edanahy22@gmail.com>
2022-06-27 19:57:37 -0500

    schema and seeds added

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO department(name)
+VALUES ("Software Design"),
+       ("Human Resources"),
+       ("Management");
+
+
+INSERT INTO e_role (title, salary)
+VALUES ("Intern", 30,000),
+       ("Manager", 150,000),
+       ("Engineer", 100,000);
+
+INSERT INTO employee (first_name, last_name)
+VALUES ("Patrick", "Star"),
+       ("Eugiene", "Krabs"),
+       ("Spongebob", "Squarepants");
+     
