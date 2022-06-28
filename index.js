518da29238ed8325c6ed12e0b81725265efd0d45
Elaine Danahy <edanahy22@gmail.com>
2022-06-28 13:19:56 -0500

    added view table functions

--- index.js
+++ index.js
@@ -1,7 +1,79 @@
-import inquirer from 'inquirer';
-// const mysql2 = require('mysql2');
+const inquirer = require('inquirer');
+const mysql2 = require('mysql2');
 // const conslTable = require('console-table');
 
+// Connect to database
+const db = mysql2.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password here
+      password: 'Finnegan23',
+      database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database.`)
+  );
+
+const compileCompany= () => {
+
+}
+
+const viewEmployees = () => {
+    const sql = `SELECT * FROM employee`;
+    db.query(sql, (err, result) => {
+      if (err) {
+        console.err(err)
+      }
+      console.log(result);
+      return result;
+    });
+}
+
+const addEmployee = () => {
+    const sql = `SELECT * FROM employee`;
+    db.query(sql, (err, result) => {
+      if (err) {
+        console.err(err)
+      }
+      console.log(result);
+      return result;
+    });
+}
+
+const updateEmployee = () => {
+
+}
+
+const viewRoles =  () => {
+    const sql = `SELECT * FROM e_role`;
+    db.query(sql, (err, result) => {
+      if (err) {
+        console.err(err)
+      }
+      console.log(result);
+      return result;
+    });
+}
+
+const addRole = () => {
+
+}
+
+const viewDepartments = () => {
+    const sql = `SELECT * FROM department`;
+    db.query(sql, (err, result) => {
+      if (err) {
+        console.err(err)
+      }
+      console.log(result);
+      return result;
+    });
+}
+
+const addDepartment = () => {
+
+}
 
 const createCompany = () => {
     inquirer
